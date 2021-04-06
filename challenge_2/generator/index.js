5f34d60e41d490cdc8a0a34dda21d0129f576ed3
calvin <calvinchenhupro@gmail.com>
2021-04-06 11:49:58 -0700

    updated

---
+++ challenge_2/generator/index.js
@@ -0,0 +1,28 @@
+
+
+// JSON to CSV_report logic - controller
+module.exports = {
+  generateCsv: function(req, res) {
+
+  var csvFile = ''
+  // recursive function to flaten the file
+  // it will return a text file
+
+  // fs.write / '/csv_file' /
+  // fs.writeFile('/csv_file', data, [encoding], [callback])
+
+  }
+
+}
+
+/*
+Object to String
+
+"firstName,lastName,county,city,role,sales \n
+Joshie,Wyattson,San Mateo,San Mateo,Broker,1000000 \n
+Beth Jr.,Johnson,San Mateo,Pacifica,Manager,2900000 \n
+Smitty,Won,San Mateo,Redwood City,Sales Person,4800000 \n
+Allen,Price,San Mateo,Burlingame,Sales Person,2500000 \n
+Beth,Johnson,San Francisco,San Francisco,Broker/Sales Person,7500000"
+
+*/
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,5 +1,6 @@
-const express = require('express')
+const express = require('express');
 const bodyParser = require('body-parser');
+const csv = require('./generator');
 
 const app = express()
 const port = 3000
@@ -14,22 +15,41 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-// route handler
+// route handler - post request
 app.post('/upload_json', urlencodedParser, (req, res) => {
   console.log(req.body);
-
   res.send('received POST!');
+
+  csv.generateCsv(req, res);
 
 })
 
-// JSON to CSV_report logic - Tree Map
-const generate = function(req, res) {
 
-}
+// route handler - get request
+app.get('/csv_file', function (req, res, next) {
+  console.log('received GET!');
 
-const create = function(params, callback) {
+  // const options = {
+  //   root: path.join(__dirname, '/csv_file'),
+  //   dotfiles: 'deny',
+  //   headers: {
+  //     'x-timestamp': Date.now(),
+  //     'x-sent': true
+  //   }
+  // }
+})
 
-}
+
+/*
+express res.sendFile()
+
+https://bezkoder.com/node-js-express-download-file/
+
+DISPLAY THE FILE
+res.end(‘<html>...<div>’ + csvString + ‘</div>....</html>’)
+
+*/
+
 
 
 app.listen(port, () => {
